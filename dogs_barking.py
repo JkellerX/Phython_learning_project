f714eb18e81feb513347b26e0e88c3583413322a
Joanna Keller <joannakeller01@gmail.com>
2024-08-19 11:57:49 +0200

    added new file dogs_barking

---
+++ dogs_barking.py
@@ -0,0 +1,19 @@
+dog_name = "Neli"
+dog_weight = 3
+if dog_weight > 10:
+    print(dog_name, 'Wof, Wof')
+
+dog_name = "Tokio"
+dog_weight = 2
+if dog_weight > 10:
+    print(dog_name, 'Wof, Wof')
+
+dog_name = "Jessie"
+dog_weight = 8
+if dog_weight > 10:
+    print(dog_name, 'Wof, Wof' )
+
+dog_name = "Rex"
+dog_weight = 6
+if dog_weight > 10:
+    print(dog_name, 'Wof, Wof')
