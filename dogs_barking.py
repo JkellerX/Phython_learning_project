52edf82a2d2ae718ec69c740f422141a7aba784f
Joanna Keller <joannakeller01@gmail.com>
2024-08-19 12:38:05 +0200

    added comments

--- dogs_barking.py
+++ dogs_barking.py
@@ -1,3 +1,5 @@
+#Przekształcenie kodu w funkcje:
+
 print('Prepare the dogs')
     
 def barking(dog_name, weight):
@@ -12,3 +14,16 @@ barking("Jessie", 8)
 barking("Rex", 15)
 
 print("ok, that's enought")
+
+#Fukcja z wartością zwrotną:
+
+def download_barking(weight):
+    if weight > 10:
+        return 'Wof, Wof'
+    else:
+        return 'wof, Wof'
+
+#Wywolanie funkcji z wartościa zwrotną:
+
+toki_barking = download_barking(40)
+print("Tokio is barking", toki_barking)
