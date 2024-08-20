25d8bfd8297472d57c48ac50eaea4d5944413f17
Joanna Keller <joannakeller01@gmail.com>
2024-08-20 13:23:51 +0200

    spacing added

--- icecream.py
+++ icecream.py
@@ -1,12 +1,12 @@
-def make_icecream_cup(icecream= 'wanilla', icing= 'chocolate', pinats=True,
+def make_icecream_cup(icecream='vanilla', icing='chocolate', pinats=True,
                       banana=True, cookies=True, whipped_cream=True):
-    receipe = 'icecream' + icecream + 'icing' + icing + ''
+    receipe = 'icecream ' + icecream + ' icing ' + icing + ' '
     if pinats:
-        receipe = receipe + 'with pinats'
+        receipe = receipe + 'with pinats '
     if banana:
-        receipe = receipe + 'banana'
+        receipe = receipe + 'banana '
     if cookies:
-        receipe = receipe + 'cookies'
+        receipe = receipe + 'cookies '
     if whipped_cream:
         receipe = receipe + 'whipped cream'
     else:
@@ -19,7 +19,7 @@ print('You ordered:', cup)
 cup = make_icecream_cup('chocolate')
 print('You ordered:', cup)
 
-cup = make_icecream_cup(icing='carmel', whipped_cream= False, banana=False)
+cup = make_icecream_cup(icing='caramel', whipped_cream=False, banana=False)
 print('You ordered:', cup)
 
 cup = make_icecream_cup(whipped_cream=False, banana=True, cookies=True, icecream='pinats')
