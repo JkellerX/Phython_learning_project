7be82f7d07146701247ccef562d62df8748206bb
Joanna Keller <joannakeller01@gmail.com>
2024-08-27 11:21:20 +0200

    added new folder

--- bubbles_sort.py
+++ bubbles_sort.py
@@ -1,10 +1,16 @@
 #Using sorting for the 5 best samples (creating pseudo code)
-def samples_sorting(items):
+
+#creating
+
+def samples_sorting(results, items):
     change = True
     while change:
         change = False
-        for i in range(0, len(items) - 1):
-            if items[i] > items[i + 1]:
+        for i in range(0, len(items)-1):
+            if results[i] < items[i + 1]:
+                temporary = items[i]
+                results[i] = results[i + 1]
+                results[i + 1] = temporary
                 temporary = items[i]
                 items[i] = items[i + 1]
                 items[i + 1] = temporary
@@ -17,9 +23,13 @@ results = [60, 50, 60, 58, 54, 54,
            46, 31, 57, 52, 44, 18,
            41, 51, 55, 61, 51, 44]
 
-samples_sorting(results)
-print(results)
+samples_sorting(results, items)
+print(results, items)
 
-coctails = ['coconut', 'strawberry', 'banana', 'pineapple']
-samples_sorting(coctails)
-print(coctails)
+
+#generating raport with the best 5 samples
+
+number_of_results = len(results)
+numbers_of_samples = list(range(number_of_results))
+
+samples_sorting(results, numbers_of_samples)
