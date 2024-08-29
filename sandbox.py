8c9050ad99c40677778ef9d3482b533f68ffd784
Joanna Keller <joannakeller01@gmail.com>
2024-08-29 12:29:14 +0200

    added new code and new folder

--- sandbox.py
+++ sandbox.py
@@ -1,25 +1,10 @@
-for i in range(0,4):
-    for j in range(0,4):
-        print (i * j)
+import ch1text
+def oblicz_czytelnosc(tekst):
+    liczba_slow = 0
+    liczba_zdan = 0
+    liczba_sylab = 0
+    wynik = 0 
 
-for wyraz in ['aa', 'bbb', 'cccc', 'dddd', 'eeeee']:
-    for i in range(2,7):
-        litery = len(wyraz)
-        if (litery % i) == 0:
-            print(i, wyraz)
+    print(tekst)
 
-pelny = False
-
-datki =[]
-limit = 45
-
-zabawki = ['robor', 'lalka', 'piłka', 'puzzle']
-
-while not pelny:
-    for zabawka in zabawki:
-        datki.append(zabawka)
-        ilosc = len(datki)
-        if (ilosc >= limit):
-            pelny = True
-print('w koszyku znajduje się', len(datki), 'zabawek')
-print(datki)
+oblicz_czytelnosc(ch1text.tekst)
