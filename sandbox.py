5f76eb81b5b881a58eccee42a84fe066ef0f93f3
Joanna Keller <joannakeller01@gmail.com>
2024-08-29 12:57:10 +0200

    added new code

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,23 @@
 import ch1text
+
+def policz_zdania(tekst):
+    liczba = 0
+    for znak in tekst:
+        if znak in '.?!':
+            liczba = liczba + 1
+    return liczba
+
 def oblicz_czytelnosc(tekst):
     liczba_slow = 0
     liczba_zdan = 0
     liczba_sylab = 0
     wynik = 0 
 
-    print(tekst)
+    slowa = tekst.split()
+    liczba_slow = len(slowa)
+    liczba_zdan = policz_zdania(tekst)
+    
+    print(liczba_slow, 'słow')
+    print(liczba_zdan, 'zdan')
 
 oblicz_czytelnosc(ch1text.tekst)
