f3ce93b2a07971aae582e6b00a32cf36fcc8315b
Joanna Keller <joannakeller01@gmail.com>
2024-08-29 11:54:23 +0200

    added new code

--- sandbox.py
+++ sandbox.py
@@ -1,37 +1,25 @@
-results = [60, 50, 60, 58, 54, 54,
-          58, 50, 52, 54, 48, 69,
-          34, 55, 51, 52, 44, 51,
-          69, 64, 66, 55, 52, 61,
-          46, 31, 57, 52, 44, 18,
-          41, 51, 55, 61, 51, 44]
+for i in range(0,4):
+    for j in range(0,4):
+        print (i * j)
 
-costs = [.25, .27, .25, .25, .25, .25,
-         .33, .31, .24, .29, .22, .22,
-         .25, .25, .28, .33, .21, .25,
-         .25, .25, .25, .28, .24, .22,
-         .20, .25, .30, .25, .24, .25,
-         .25, .25, .27, .26, .26, .29]
+for wyraz in ['aa', 'bbb', 'cccc', 'dddd', 'eeeee']:
+    for i in range(2,7):
+        litery = len(wyraz)
+        if (litery % i) == 0:
+            print(i, wyraz)
 
-najwyzszy_wynik = 0 
-lenght = len(results)
-for i in range(lenght):
-    print('wynik ' + str(i), ' to', results[i])
-    if results[i] > results[i]:
-        najwyzszy_wynik = results[i]
-    print('liczba testow', lenght)
-    print('najwyzszy wynik', najwyzszy_wynik)
+pelny = False
 
-    najepsze_roztwory = []
-    for i in range(lenght):
-        if najwyzszy_wynik == results[i]:
-           najepsze_roztwory.append(i)
-    print('najlepsze wyniki:', najepsze_roztwory)
+datki =[]
+limit = 45
 
-koszt = 100.0
-najbardziej_oplacalny = 0 
-for i in range(lenght):
-    if results[i] == najwyzszy_wynik and costs[i] < koszt:
-        najbardziej_oplacalny = i
-        koszt = costs[i] 
-print('roztwor o najwyzszym wyniku i najnizszych kosztach:', najbardziej_oplacalny,
-       'kosztuje:', costs[najbardziej_oplacalny])
+zabawki = ['robor', 'lalka', 'piłka', 'puzzle']
+
+while not pelny:
+    for zabawka in zabawki:
+        datki.append(zabawka)
+        ilosc = len(datki)
+        if (ilosc >= limit):
+            pelny = True
+print('w koszyku znajduje się', len(datki), 'zabawek')
+print(datki)
