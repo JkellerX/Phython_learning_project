7f81bd80ef400f958cb934ff8e0d649775548607
Joanna Keller <joannakeller01@gmail.com>
2024-09-06 11:29:38 +0200

    added new folder

--- python_race/turtle_race.py
+++ python_race/turtle_race.py
@@ -5,7 +5,11 @@ zolwie = list()
 
 def ustawienia():
     global zolwie
-    start = -480
+    start = -620
+    ekran = turtle.Screen()
+    ekran.setup(1290,720)
+    ekran.bgpic('pavement.gif')
+
 
     zolw_pion = [-40, -20, 0, 20, 40]
     zolw_kolor = ['blue', 'red', 'purple', 'brown', 'gray']
@@ -13,8 +17,10 @@ def ustawienia():
     for i in range(0, len(zolw_pion)):
         nowy_zolw = turtle.Turtle()
         nowy_zolw.shape('turtle')
+        nowy_zolw.penup()
         nowy_zolw.setpos(start, zolw_pion[i])
         nowy_zolw.color(zolw_kolor[i])
+        nowy_zolw.pendown()
         zolwie.append(nowy_zolw)
 ustawienia()
 turtle.mainloop()
