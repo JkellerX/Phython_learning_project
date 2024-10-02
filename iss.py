8d088443025cb58b6ad26306e8ac014b03bd0929
Joanna Keller <joannakeller01@gmail.com>
2024-10-02 10:17:44 +0200

    added new code

--- iss.py
+++ iss.py
@@ -2,7 +2,9 @@ import requests, json, turtle
 url = 'http://api.open-notify.org/iss-now.json'
 odpowiedz = requests.get(url)
 if (odpowiedz.status_code == 200):
-    print(odpowiedz.text)
+    odpowiedz_slownik = json.loads(odpowiedz.text)
+    polozenie = odpowiedz_slownik['iss_position']
+    print('Wspolrzedne ISS to' + polozenie['latitude'] + ' ,' + polozenie['longitude'])
 else:
     print("Huston, mamy problem:", odpowiedz.status_code)
-    
+
